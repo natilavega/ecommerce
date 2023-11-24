093ddadfff88cfe005a4215bd4c9bb38d34a18a6
Nati Lavega <nati.lavega@gmail.com>
2023-11-24 11:58:50 -0300

    feat: agrega listado de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import { Products } from "./components/Products"
+import { products } from './mocks/products.json'
+
 function App() {
   return (
     <>
       <h1>React Shop</h1>
+      <Products products={products} />
     </>
   )
 }
