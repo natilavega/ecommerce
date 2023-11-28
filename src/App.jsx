6b0f407478ff473f1849eb50688749c666bc1963
Nati Lavega <nati.lavega@gmail.com>
2023-11-28 09:44:29 -0300

    feat: agrega filtros de precio y categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
+import { useState } from 'react'
+import { Header } from './components/Header'
 import { Products } from "./components/Products"
-import { products } from './mocks/products.json'
+import { products as initialProducts } from './mocks/products.json'
 
 function App() {
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && (
+          filters.category === 'all' || product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <>
-      <h1>React Shop</h1>
-      <Products products={products} />
+      <Header changeFilters={setFilters} />
+      <Products products={filteredProducts} />
     </>
   )
 }
